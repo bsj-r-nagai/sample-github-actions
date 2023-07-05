5f0b41ddd9234f12515121110f730cd790c66397
r.nagai <r.nagai@bravesoft.co.jp>
2023-07-05 15:05:06 +0900

    new

---
+++ SampleGitHubActions/SampleGitHubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGitHubActionsApp.swift
+//  SampleGitHubActions
+//
+//  Created by 永井涼 on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct SampleGitHubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
